6b9b5ba476f40d917773775cd6d3522725f89a73
jomarlopezlorigas <jomarlopezlorigas>
2026-03-06 14:21:30 +0800

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
             <ProjectCard
               title="Sentra - Head Monitoring System"
               description="AI cheating detection using head tracking"
-            
+              repo=""
               image="/Projects/Sentra.png"
             />
           </div>
